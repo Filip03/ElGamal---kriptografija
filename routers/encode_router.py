d2b6e78bcab749a3c4f46db8788a2573d879577e
Filip Zejak <filipz@Filips-MacBook-Air.local>
2026-05-23 10:48:46 +0200

    spojene funkcije za enkripciju i elgam_set

--- routers/encode_router.py
+++ routers/encode_router.py
@@ -2,22 +2,27 @@ from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 
 from functions.elgam_enc import elgam_enc
+from functions.elgam_set import elgam_set
 
 router = APIRouter()
 
 class EncryptionReq(BaseModel):
     mssg: str
-    p: str
-    g: str
-    ga: str
+    k: int
 
 class EncryptionResponse(BaseModel):
     cipher: list[tuple[str, str]]
+    p: str
+    g: str
+    a: str
+    ga: str
 
 @router.get("")
 def encrypt(req: EncryptionReq):
     try:
-        result = elgam_enc(req.mssg, req.p, req.g, req.ga)
-        return EncryptionResponse(cipher=[(str(c1), str(c2)) for c1, c2 in result])
+        data = elgam_set(req.k)
+        result = elgam_enc(req.mssg, data.p, data.g, data.ga)
+        return EncryptionResponse(cipher=[(str(c1), str(c2)) for c1, c2 in result], p = data.p, g = data.g,
+                                a = data.a, ga = data.ga)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
